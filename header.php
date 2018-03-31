73bec0ac5dd285e3f1307b4f96ff7245da32ea73
Govind <you@example.com>
2018-03-31 18:14:10 +0530

    higher lower

---
+++ header.php
@@ -0,0 +1,161 @@
+
+<?php
+/*(\ccxt\Exchange::$exchanges); */
+include 'ccxt.php';
+$huobiObj   	= new \ccxt\huobi(); // BTC/CNY , LTC/CNY
+$okexObj   		= new \ccxt\okex(); // DASH/BTC ,LTC/BTC
+$liquiObj   	= new \ccxt\liqui(); // 1ST/BTC 1ST/ETH LTC/BTC DASH/BTC STEEM/BTC
+$hitbtcObj  	= new \ccxt\hitbtc(); // 1ST/BTC 1ST/ETH LTC/BTC DASH/BTC STEEM/BTC
+$binanceObj   	= new \ccxt\binance(); // LTC/BTC STEEM/BTC DASH/BTC
+/*top spreads*/
+//BTC/USD
+//$poloniexObj   	= new \ccxt\poloniex();  not free
+//$biboxObj   	= new \ccxt\bibox();   not free
+$kucoinObj   	= new \ccxt\kucoin();   // BTC/USDT
+$zbObj   		= new \ccxt\zb();           // BTC/USDT
+$gateioObj   	= new \ccxt\gateio();   // BTC/USDT  
+$tidexObj   	= new \ccxt\tidex();    // BTC/USDT  
+$cryptopiaObj 	= new \ccxt\cryptopia();  // BTC/USDT  
+$exmoObj   		= new \ccxt\exmo();    		// BTC/USDT  
+$bittrexObj  	= new \ccxt\bittrex(); 	// BTC/USDT  
+/*highest pair USD*/ 
+
+
+$kucoinObj   	= new \ccxt\kucoin();   // BTC/USDT
+$zbObj   		= new \ccxt\zb();           // BTC/USDT
+$gateioObj   	= new \ccxt\gateio();   // BTC/USDT  
+$tidexObj   	= new \ccxt\tidex();    // BTC/USDT  
+$cryptopiaObj 	= new \ccxt\cryptopia();  // BTC/USDT  
+$exmoObj   		= new \ccxt\exmo();    		// BTC/USDT  
+$bittrexObj  	= new \ccxt\bittrex(); 	// BTC/USDT  
+
+
+$higherExchange = 0;
+$lowerExchange  = 999999999999;
+$higherCurrency = '';
+$lowerCurrency  = '';
+
+
+
+
+function setHigherLower($Tickers,$currency){
+		global $higherExchange;
+		global $lowerExchange;
+		global $higherCurrency;
+		global $lowerCurrency;
+		$Tickers = number_format($Tickers['last'], 10, '.', '');
+		if($higherExchange < $Tickers){
+			$higherExchange = $Tickers; 
+			$higherCurrency = $currency;
+		}
+		if($lowerExchange > $Tickers){
+			$lowerExchange 	= $Tickers; 
+			$lowerCurrency 	= $currency;
+		}
+}
+
+
+
+function getSpreadPercentage($exchnageAprice,$exchnageBprice){
+		$spreaVolume	= ($exchnageAprice / 100 * ($exchnageAprice - $exchnageBprice));
+		$spreaVolume = number_format($spreaVolume, 10, '.', '');
+		return $spreaVolume; 
+}
+
+
+
+
+
+
+function getLastPriceOfExchage($exchange,$pair){
+		
+		global $huobiObj;
+		global $okexObj;
+		global $liquiObj;
+		global $hitbtcObj;
+		global $binanceObj;
+		global $bitmexObj;
+		
+
+		/*highest pair USDT*/
+		global $kucoinObj;
+		global $bittrexObj;
+		global $gateioObj;
+		global $tidexObj;
+		global $cryptopiaObj;
+		global $zbObj;
+		global $exmoObj;
+
+
+
+
+
+
+
+		if($exchange == 'huobi'){
+			$Tickers = $huobiObj->fetch_ticker($pair);
+		}else if($exchange == 'okex'){
+			$Tickers = $okexObj->fetch_ticker($pair);
+		}else if($exchange == 'liqui'){
+			$Tickers = $liquiObj->fetch_ticker($pair);
+
+		}else if($exchange == 'hitbtc'){
+			$Tickers = $hitbtcObj->fetch_ticker($pair);
+		}else if($exchange == 'bitmex'){
+			$Tickers = $bitmexObj->fetch_ticker($pair);
+		}
+
+
+
+		else if($exchange == 'kucoin'){
+			$Tickers = $kucoinObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'kucoin');
+			
+
+
+		}else if($exchange == 'bittrex'){
+			$Tickers = $bittrexObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'bittrex');
+
+
+		}else if($exchange == 'gateio'){
+			$Tickers = $gateioObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'gateio');
+
+
+		}else if($exchange == 'tidex'){
+			$Tickers = $tidexObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'tidex');
+
+
+		}else if($exchange == 'cryptopia'){
+			$Tickers = $cryptopiaObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'cryptopia');
+
+		}else if($exchange == 'zb'){
+			$Tickers = $zbObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'zb');
+		}
+		else if($exchange == 'exmo'){
+			$Tickers = $exmoObj->fetch_ticker($pair);
+			setHigherLower($Tickers,'exmo');
+		}
+		else{
+			$Tickers = $binanceObj->fetch_ticker($pair);
+		}
+
+		$lastPrice = number_format($Tickers['last'], 10, '.', '');
+		return $lastPrice;
+}
+
+
+
+
+ 
+
+?>
+
+
+
+
+ 
--- index.php
+++ index.php
@@ -1,163 +1,6 @@
-
 <?php
-/*csss*/
-
-/*$tickershuobi = $huobiObj->load_markets();   used to load all markets*/
-
-include 'ccxt.php';
-//echo json_encode((\ccxt\Exchange::$exchanges)); 
-
-$huobiObj   	= new \ccxt\huobi(); // BTC/CNY , LTC/CNY
-$okexObj   		= new \ccxt\okex(); // DASH/BTC ,LTC/BTC
-$liquiObj   	= new \ccxt\liqui(); // 1ST/BTC 1ST/ETH LTC/BTC DASH/BTC STEEM/BTC
-$hitbtcObj  	= new \ccxt\hitbtc(); // 1ST/BTC 1ST/ETH LTC/BTC DASH/BTC STEEM/BTC
-$binanceObj   	= new \ccxt\binance(); // LTC/BTC STEEM/BTC DASH/BTC
-
-
-
- 
-
-
-
-/*
-$huobiMarket 	=	 $huobiObj->load_markets();
-$okexMarket 	=	 $okexObj->load_markets();
-$liquiMarket 	=	 $liquiObj->load_markets();
-$hitbtcMarket 	=	 $hitbtcObj->load_markets();
-$binanceMarket 	=	 $binanceObj->load_markets();
-
-$exchaneCurrency = ['huobi','okex','liqui','hitbtc','binance'];
-
-
-
-
-
-foreach ($huobiMarket as $key => $value) {
-		if (!in_array($key, $allSymbolArrys)){
-				array_push($allSymbolArrys, $key);
-		}
-}
-
-foreach ($okexMarket as $key => $value) {
-		if (!in_array($key, $allSymbolArrys)){
-				array_push($allSymbolArrys, $key);
-		}
-}
-foreach ($liquiMarket as $key => $value) {
-		if (!in_array($key, $allSymbolArrys)){
-				array_push($allSymbolArrys, $key);
-		}
-}
-
-foreach ($hitbtcMarket as $key => $value) {
-		if (!in_array($key, $allSymbolArrys)){
-				array_push($allSymbolArrys, $key);
-		}
-}
-
-
-foreach ($binanceMarket as $key => $value) {
-		if (!in_array($key, $allSymbolArrys)){
-				array_push($allSymbolArrys, $key);
-		}
-}
-
-///array_key_exists("BC/CNY",$huobiMarket)
-/*
-foreach ($huobiSymbol as $result) {
-	 echo $result['symbol'];
-}
-*/
- 
-
- 
-
-
-/*
-print_r($huobiCurrency['BTC/CNY']);
-
-echo $huobiCurrency['symbol'];
-
-
-echo '<br>';
-
-/*
-echo json_encode($okexObj->load_markets());
-echo '<br>';
-
-
-echo json_encode($liquiObj->load_markets());
-echo '<br>';
-
-echo json_encode($hitbtcObj->load_markets());
-echo '<br>';
-
-echo json_encode($binanceObj->load_markets());
-echo '<br>';
-*/
-
-
-
-
-
-/*
-
-$dataArray = array('LTC/BTC','STEEM/BTC','SBD/BTC','DASH/BTC','DCT/BTC');
- 
-$tickersLiqui	= 	$liquiObj->fetch_ticker($dataArray[0]);
-$tickersHitBtc	= 	$hitbtcObj->fetch_ticker($dataArray[0]);
-//
-$tickersLiquiSTEEM	= 	$liquiObj->fetch_ticker($dataArray[1]);
-$tickersHitBtcSTEEM	= 	$hitbtcObj->fetch_ticker($dataArray[1]);
-//
-$tickersLiquiSBD	= 	$liquiObj->fetch_ticker($dataArray[2]);
-$tickersHitBtcSBD	= 	$hitbtcObj->fetch_ticker($dataArray[2]);
-//
-$tickersLiquiDASH	= 	$liquiObj->fetch_ticker($dataArray[3]);
-$tickersHitBtcDASH	= 	$hitbtcObj->fetch_ticker($dataArray[3]);
-//
-$tickersLiquiDCT	= 	$liquiObj->fetch_ticker($dataArray[4]);
-$tickersHitBtcDCT	= 	$hitbtcObj->fetch_ticker($dataArray[4]);
-
-*/
-
-
-function getSpreadPercentage($exchnageAprice,$exchnageBprice){
-		$spreaVolume	= ($exchnageAprice / 100 * ($exchnageAprice - $exchnageBprice));
-		$spreaVolume = number_format($spreaVolume, 20, '.', '');
-		return $spreaVolume; 
-}
-
-
-
-function getLastPriceOfExchage($exchange,$pair){
-		
-		global $huobiObj;
-		global $okexObj;
-		global $liquiObj;
-		global $hitbtcObj;
-		global $binanceObj;
-		
-		if($exchange == 'huobi'){
-			$Tickers = $huobiObj->fetch_ticker($pair);
-
-		}else if($exchange == 'okex'){
-			$Tickers = $okexObj->fetch_ticker($pair);
-
-		}else if($exchange == 'liqui'){
-			$Tickers = $liquiObj->fetch_ticker($pair);
-
-		}else if($exchange == 'hitbtc'){
-			$Tickers = $hitbtcObj->fetch_ticker($pair);
-		}else{
-			$Tickers = $binanceObj->fetch_ticker($pair);
-		}
-
-		$lastPrice = number_format($Tickers['last'], 20, '.', '');
-		return $lastPrice;
-}
+	include 'header.php';
 ?>
-
 
 <!--CSSSSS-->
 <!DOCTYPE html>
@@ -173,15 +16,120 @@ function getLastPriceOfExchage($exchange,$pair){
     <link href='/css/fonts.css' media='screen' rel='stylesheet' type='text/css'>
     <link href='/css/responsive.css' media='screen' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+
+
+
+
     <script src="/js/app.js"></script>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	 
+
+
+	<script type="text/javascript" language="javascript">  
+		/*
+		$(window).load(function() {
+		 // executes when complete page is fully loaded, including all frames, objects and images
+		  $.ajax({  
+	             type:"POST",
+	             data: {requiest: "success",},  
+	             url:"higherexchange.php",  
+	             success:function(data){  
+	                alert(data);  
+	             }  
+     		});
+		});
+		 $(window).load(function(){
+		 	$.ajax({  
+		             type:"POST",  
+		             data: {requiest: "success",},  
+		             url:"higherexchange.php",  
+		             success:function(data){  
+		              	var finaldata = $.parseJSON(data);	
+
+		              	alert('lowerExchange Data '+finaldata.lowerExchange);
+
+		                
+		             }  
+		     });
+		 });  
+*/
+</script>   
+
+
+</head>
 
 <body>
 		<div class="container">
 				<h2 align="center">Token Spreads</h2>
 				<h4 align="center">crypto Spread Monitoring And Alerts</h4>
 			
+
+		
+		<div class="col-xs-12">	
+				<h1><i class="material-icons">Top</i>Top Spreads</h1>
+		</div>
+
+		<div class="col-md-12">			
+
+
+				<div class="col-md-4">
+				    <table id="latest-btc">
+				       <tr>
+				         <th colspan="2">Latest BTC/USDT Price Spreads</th>
+				       </tr>
+				       <tr>
+				            <td>kucoin</td>
+				            <td><?php echo getLastPriceOfExchage('kucoin','BTC/USDT');?></td>
+				       </tr>
+				       <tr>          
+				      		<td>bittrex</td> 
+				      		<td><?php echo getLastPriceOfExchage('bittrex','BTC/USDT');?></td>
+				       </tr>
+				       <tr> 
+				      		<td>gateio</td>  
+				      		<td><?php echo getLastPriceOfExchage('gateio','BTC/USDT');?></td>
+				       </tr>
+				       <tr>          
+				      		<td>tidex</td>
+				      		<td><?php echo getLastPriceOfExchage('tidex','BTC/USDT');?></td>
+				       </tr>
+				       <tr>  <td>cryptopia</td>
+				             <td><?php echo getLastPriceOfExchage('cryptopia','BTC/USDT');?></td>
+				        </tr>
+				        <tr>  
+				        	  <td>zb</td>
+				        	  <td><?php echo getLastPriceOfExchage('zb','BTC/USDT');?></td>
+				        </tr>
+				        <tr>   
+				        		<td>exmo</td>
+				        		<td><?php echo getLastPriceOfExchage('exmo','BTC/USDT');?></td>
+				        </tr>
+				    </table>
+				  </div>
+
+				  <div class="col-md-4">
+
+				  			<table id="latest-spread-price">
+						        <tr>
+						          <th colspan="2">High Price Spread</th>
+						        </tr>
+						      		<tr>      
+						      			<td class="latest-feature data-price-max"> 
+						      				<span class="latest-exchange">Low Exchange: <strong><?php echo $lowerCurrency;?></strong>
+
+						      				<br>High Exchange: <strong><?php echo $higherCurrency; ?></strong></span>      
+						      			</td>
+
+						      		</tr>
+						    </table>
+
+				  </div>
+
+
+		</div>
+
+
 			<div class="col-xs-12">	
 				<h1><i class="material-icons">tune</i> Exchange Spreads</h1>
 			</div>
@@ -733,25 +681,16 @@ function getLastPriceOfExchage($exchange,$pair){
 									 <td>Currency Pair</td> 
 									 <td>Currency Pair</td> 
 								</tr>
-
-
-
-
-
-
-
-
-
 <!--STEEM/BTC-->
-								
-
-
 				</table>
-			
-
 		</div>
 </body>
+
 </html>
+
+
+
+
 
 
 
